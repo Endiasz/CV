1dd4f0c5a9649141a3936f4605b3d302802e8688
Endiasz <endiaszpl@gmail.com>
2023-03-10 00:26:16 +0100

    dostałem raka exe

--- steam_Api_BrokenArrow/api.py
+++ steam_Api_BrokenArrow/api.py
@@ -9,10 +9,9 @@ class news:
         self.title = title
         self.content = content
         self.url = url
-        
 
     def getfull(self):
-        return (self.id, self.title, self.content)
+        return (self.gid, self.title, self.content, self.url)
 
     def get_gid(self):
         return self.gid
@@ -22,22 +21,22 @@ class news:
 
     def get_Title(self):
         return self.title
-    
+
     def get_url(self):
         return self.url
-
 
     def display(self):
         print(f"\nTitle: {self.title}\nContent:\n{self.content}\n\n{self.url}")
 
 
-req = requests.get('https://api.steampowered.com/ISteamNews/GetNewsForApp/v0002/?appid=1604270&count=3&maxlength=600&format=json') # returning json
+req = requests.get(
+    'https://api.steampowered.com/ISteamNews/GetNewsForApp/v0002/?appid=1604270&count=3&maxlength=600&format=json')  # returning json
 response = json.loads(req.text)
 container = response['appnews']
 
 news_List = []
 
-for item in container['newsitems']:    
+for item in container['newsitems']:
     gid = item['gid']
     title = item['title']
     content = item['contents']
@@ -47,11 +46,45 @@ for item in container['newsitems']:
     news_List.append(news_Item)
 
 
-# for i in news_List:
-#     if(i.get_gid()=='5059268099240480567'): # if new item 
-#         # display
-#         i.display()
+#  Checking for new news. Side of code
 
-news_List[0].display()
+previously_gathered = []
+
+f = open("news.txt")
 
 
+gathered = []
+
+if(f.read() == None or f.read() == ''):
+# if(True):
+    print('saveing data')
+    for i in news_List:
+        gid, title, content, url = i.getfull()
+        item = {'gid': gid, 'title': title, 'content': content, 'url': url}
+
+        gathered.append(item)
+        # print(item)
+        
+    f.close()
+    
+    f = open("news.txt",'w')
+    f.write(json.dumps(gathered))
+    f.close()
+
+else:
+    print("Loadnig json")
+    
+    try:
+        prev = json.load(f.read())
+    except:
+        print('Failed to load json')
+
+# print(gathered)
+
+
+# print(news_List[0])
+
+# json.
+
+
+# news_List[0].display() # displat missing
